438aceae81ded255c32829336a7e870484c38b16
pigi5 <ford.hash@gmail.com>
2018-08-31 11:06:08 -0500

    fixed template linking error

--- GamingFrameworksEngine/header/Utils.h
+++ GamingFrameworksEngine/header/Utils.h
@@ -2,6 +2,12 @@
 
 namespace engine_util
 {
-    // Implements the signum function
-    template <typename T> int sign(T val);
+    // Implements the signum function.
+    // params:
+    //   val - the value to check sign of
+    // returns: -1 if val is negative, 0 if val is 0, 1 if val is positive
+    template <typename T> int sign(T val)
+    {
+        return (T(0) < val) - (val < T(0));
+    }
 }
