3272c73fc1d3166220082d17700e28a09786d452
discospider13 <matt_engman@baylor.edu>
2018-09-03 23:15:31 -0500

    Window Handler indirection

--- GamingFrameworksEngine/header/WindowHandler.h
+++ GamingFrameworksEngine/header/WindowHandler.h
@@ -9,21 +9,64 @@ class WindowHandler {
 private:
 	int fullScreen;
 	int windowWidth, windowHeight;
+	sf::Font defaultFont = sf::Font();
 	string windowName;
 	sf::VideoMode currentMode = sf::VideoMode(800,600);
 	vector<sf::VideoMode> fullValid = currentMode.getFullscreenModes();
 	sf::RenderWindow window;
+	vector<sf::Text> toDrawText;
+	vector<sf::Shape> toDrawShape;
+	vector<sf::VertexArray> toDrawVList;
+	vector<sf::Sprite> toDrawSprite;
 public:
 	WindowHandler();
 	WindowHandler(string name, int width, int height);
 	WindowHandler(string name, bool fullscreen);
 
-	int getWinWidth();
-	int getWinHeight();
+	int getViewWidth();
+	int getViewHeight();
+	int getWindowWidth();
+	int getWindowHeight();
 	string getWinName();
-	void setWinWidth(int width);
-	void setWinHeight(int height);
+
+	void setViewWidth(int width);
+	void setViewHeight(int height);
 	void setWinName(string name);
+
+	int addText(string txt, int xPos, int yPos);
+	void updateText(int id, string txt);
+	void setTextPosition(int id, int xPos, int yPos);
+	void moveText(int id, int dx, int dy);
+	void rotateText(int id, int angle);
+	void setTextSize(int id, int size);
+	void setTextFont(int id, sf::Font font);
+	void setTextStyle(int id, char style);
+	void setTextColor(int id, sf::Color color);
+
+	int addShape(int numPoints, int xPos, int yPos);
+	int addCustomShape(sf::Shape& shape);
+	void setShapePosition(int id, int xPos, int yPos);
+	void moveShape(int id, int dx, int dy);
+	void rotateShape(int id, int angle);
+	void setShapeSize(int id, int sizeX, int sizeY);
+	void setShapeColorFill(int id, sf::Color color);
+	void setShapeColorOutline(int id, sf::Color color);
+	void setShapeOutlineThickness(int id, int size);
+	void setShapeTexture(int id, string filename, int xSize, int ySize);
+
+	int addVertexList(sf::PrimitiveType type, int vertexCount, sf::Vertex vList[]);
+	void changeVertexListType(int id, sf::PrimitiveType type);
+	void changeVertexPosition(int id, int index, int posX, int posY);
+	void changeVertexColor(int id, int index, sf::Color color);
+	void addVertex(int id, sf::Vertex v);
+
+	int addSprite(string filename, int xPos, int yPos, int xSize, int ySize);
+	void setSpritePosition(int id, int xPos, int yPos);
+	void moveSprite(int id, int dx, int dy);
+	void rotateSprite(int id, int angle);
+	void setSpriteSize(int id, int sizeX, int sizeY);
+	void setSpriteTexture(int id, string filename, int xSize, int ySize);
+
 	void toggleFullScreen();
-	void openWindow(vector<sf::Drawable> toDraw);
+	void openWindow();
 };
--- GamingFrameworksEngine/src/WindowHandler.cpp
+++ GamingFrameworksEngine/src/WindowHandler.cpp
@@ -6,43 +6,192 @@ WindowHandler::WindowHandler() {
 	this->windowWidth = 800;
 	this->windowHeight = 600;
 	this->fullScreen = -1;
+	defaultFont.loadFromFile("arial.ttf");
 }
 WindowHandler::WindowHandler(string name, int width, int height) {
 	this->setWinName(name);
-	this->setWinWidth(width);
-	this->setWinHeight(height);
+	this->setViewWidth(width);
+	this->setViewHeight(height);
 	this->fullScreen = -1;
 	this->currentMode = sf::VideoMode(windowWidth, windowHeight);
+	defaultFont.loadFromFile("arial.ttf");
 }
 WindowHandler::WindowHandler(string name, bool fullscreen) {
 	this->setWinName(name);
 	this->fullScreen = 1;
+	defaultFont.loadFromFile("arial.ttf");
 }
 
-int WindowHandler::getWinWidth() {
+int WindowHandler::getViewWidth() {
 	return windowWidth;
 }
-int WindowHandler::getWinHeight() {
+int WindowHandler::getViewHeight() {
 	return windowHeight;
+}
+int WindowHandler::getWindowWidth() {
+	return fullValid[0].width;
+}
+int WindowHandler::getWindowHeight() {
+	return fullValid[0].height;
 }
 string WindowHandler::getWinName() {
 	return windowName;
 }
-void WindowHandler::setWinWidth(int width) {
+
+void WindowHandler::setViewWidth(int width) {
 	this->windowWidth = width;
 	this->currentMode = sf::VideoMode(windowWidth, windowHeight);
 }
-void WindowHandler::setWinHeight(int height) {
+void WindowHandler::setViewHeight(int height) {
 	this->windowHeight = height;
 	this->currentMode = sf::VideoMode(windowWidth, windowHeight);
 }
 void WindowHandler::setWinName(string name) {
 	this->windowName = name;
 }
+
+int WindowHandler::addText(string txt, int xPos, int yPos) {
+	sf::Text text = sf::Text(txt, defaultFont, txt.size);
+	text.setPosition(xPos, yPos);
+	toDrawText.push_back(text);
+	return toDrawText.size() - 1;
+}
+void WindowHandler::updateText(int id, string txt) {
+	toDrawText[id].setString(txt);
+}
+void WindowHandler::setTextPosition(int id, int xPos, int yPos) {
+	toDrawText[id].setPosition(xPos, yPos);
+}
+void WindowHandler::moveText(int id, int dx, int dy) {
+	toDrawText[id].move(dx, dy);
+}
+void WindowHandler::rotateText(int id, int angle) {
+	toDrawText[id].rotate(angle);
+}
+void WindowHandler::setTextSize(int id, int size) {
+	toDrawText[id].setCharacterSize(size);
+}
+void WindowHandler::setTextFont(int id, sf::Font font) {
+	toDrawText[id].setFont(font);
+}
+void WindowHandler::setTextStyle(int id, char style) {
+	switch (style)
+	{
+	case 'r': toDrawText[id].setStyle(sf::Text::Regular);
+		break;
+	case 'b': toDrawText[id].setStyle(sf::Text::Bold);
+		break;
+	case 'i': toDrawText[id].setStyle(sf::Text::Italic);
+		break;
+	case 's': toDrawText[id].setStyle(sf::Text::StrikeThrough);
+		break;
+	}
+}
+void WindowHandler::setTextColor(int id, sf::Color color) {
+	toDrawText[id].setFillColor(color);
+}
+
+int WindowHandler::addShape(int numPoints, int xPos, int yPos) {
+	if (numPoints == 4) {
+		sf::RectangleShape s = sf::RectangleShape(sf::Vector2f(1, 1));
+		s.setPosition(xPos, yPos);
+		toDrawShape.push_back(s);
+		return toDrawShape.size - 1;
+	}
+	else {
+		sf::CircleShape s = sf::CircleShape(0.5, numPoints);
+		s.setPosition(xPos, yPos);
+		toDrawShape.push_back(s);
+		return toDrawShape.size - 1;
+	}
+	
+}
+int WindowHandler::addCustomShape(sf::Shape& shape) {
+	toDrawShape.push_back(shape);
+	return toDrawShape.size - 1;
+}
+void WindowHandler::setShapePosition(int id, int xPos, int yPos) {
+	toDrawShape[id].setPosition(xPos, yPos);
+}
+void WindowHandler::moveShape(int id, int dx, int dy) {
+	toDrawShape[id].move(dx, dy);
+}
+void WindowHandler::rotateShape(int id, int angle) {
+	toDrawShape[id].rotate(angle);
+}
+void WindowHandler::setShapeSize(int id, int sizeX, int sizeY) {
+	toDrawShape[id].setScale(sizeX, sizeY);
+}
+void WindowHandler::setShapeColorFill(int id, sf::Color color) {
+	toDrawShape[id].setFillColor(color);
+}
+void WindowHandler::setShapeColorOutline(int id, sf::Color color) {
+	toDrawShape[id].setOutlineColor(color);
+}
+void WindowHandler::setShapeOutlineThickness(int id, int size) {
+	toDrawShape[id].setOutlineThickness(size);
+}
+void WindowHandler::setShapeTexture(int id, string filename, int xSize, int ySize) {
+	sf::Texture* t = new sf::Texture();
+	sf::IntRect r = sf::IntRect(sf::Vector2i(toDrawShape[id].getPosition().x, toDrawShape[id].getPosition().y), sf::Vector2i(xSize, ySize));
+	t->loadFromFile(filename, r);
+	toDrawShape[id].setTexture(t, true);
+}
+
+int WindowHandler::addVertexList(sf::PrimitiveType type, int vertexCount, sf::Vertex vList[]) {
+	sf::VertexArray va = sf::VertexArray(type, vertexCount);
+	for (int i = 0; i < vertexCount; i++) {
+		va[i] = vList[i];
+	}
+	toDrawVList.push_back(va);
+	return toDrawVList.size() - 1;
+}
+void WindowHandler::changeVertexListType(int id, sf::PrimitiveType type) {
+	toDrawVList[id].setPrimitiveType(type);
+}
+void WindowHandler::changeVertexPosition(int id, int index, int posX, int posY) {
+	toDrawVList[id][index].position.x = posX;
+	toDrawVList[id][index].position.y = posY;
+}
+void WindowHandler::changeVertexColor(int id, int index, sf::Color color) {
+	toDrawVList[id][index].color = color;
+}
+void WindowHandler::addVertex(int id, sf::Vertex v) {
+	toDrawVList[id].append(v);
+}
+
+int WindowHandler::addSprite(string filename, int xPos, int yPos, int xSize, int ySize) {
+	sf::Texture t = sf::Texture();
+	sf::IntRect r = sf::IntRect(sf::Vector2i(xPos, yPos), sf::Vector2i(xSize, ySize));
+	t.loadFromFile(filename, r);
+	sf::Sprite s = sf::Sprite(t, r);
+	toDrawSprite.push_back(s);
+	return toDrawSprite.size() - 1;
+}
+void WindowHandler::setSpritePosition(int id, int xPos, int yPos) {
+	toDrawSprite[id].setPosition(xPos, yPos);
+}
+void WindowHandler::moveSprite(int id, int dx, int dy) {
+	toDrawSprite[id].move(dx, dy);
+}
+void WindowHandler::rotateSprite(int id, int angle) {
+	toDrawSprite[id].rotate(angle);
+}
+void WindowHandler::setSpriteSize(int id, int sizeX, int sizeY) {
+	toDrawSprite[id].setScale(sizeX, sizeY);
+}
+void WindowHandler::setSpriteTexture(int id, string filename, int xSize, int ySize) {
+	sf::Texture t = sf::Texture();
+	sf::IntRect r = sf::IntRect(sf::Vector2i(toDrawShape[id].getPosition().x, toDrawShape[id].getPosition().y), sf::Vector2i(xSize, ySize));
+	t.loadFromFile(filename, r);
+	toDrawSprite[id].setTexture(t);
+	toDrawSprite[id].setTextureRect(r);
+}
+
 void WindowHandler::toggleFullScreen() {
 	this->fullScreen *= -1;
 }
-void WindowHandler::openWindow(vector<sf::Drawable> toDraw) {
+void WindowHandler::openWindow() {
 	if (fullScreen) {
 		window.create(fullValid[0], windowName, sf::Style::Fullscreen);
 	}
@@ -60,8 +209,17 @@ void WindowHandler::openWindow(vector<sf::Drawable> toDraw) {
 
 		window.clear(sf::Color::Black);
 
-		for (int i = 0; i < toDraw.size(); i++) {
-			window.draw(toDraw[i]);
+		for (int i = 0; i < toDrawText.size(); i++) {
+			window.draw(toDrawText[i]);
+		}
+		for (int i = 0; i < toDrawSprite.size(); i++) {
+			window.draw(toDrawText[i]);
+		}
+		for (int i = 0; i < toDrawShape.size(); i++) {
+			window.draw(toDrawText[i]);
+		}
+		for (int i = 0; i < toDrawVList.size(); i++) {
+			window.draw(toDrawText[i]);
 		}
 
 		window.display();
